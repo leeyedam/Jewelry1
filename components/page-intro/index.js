6d6812be792c6ee4b2dae870fb0aebb31949ad4c
leeyedam <dldpeka0909@gmail.com>
2023-06-09 17:04:31 +0900

    배너 슬라이드 수정

--- components/page-intro/index.js
+++ components/page-intro/index.js
@@ -1,19 +1,36 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, {EffectFade, Navigation} from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  EffectFade,
+  Navigation,
+  Autoplay,
+  Pagination,
+} from "swiper";
 
-SwiperCore.use([EffectFade, Navigation]);
+SwiperCore.use([EffectFade, Navigation, Autoplay]);
 
 const PageIntro = () => {
-
   return (
-    <section className="page-intro">  
-      <Swiper navigation effect="fade" className="swiper-wrapper">
+    <section className="page-intro">
+      <Swiper
+        navigation
+        className="swiper-wrapper"
+        slidesPerView={1}
+        autoplay={{ delay: 4000, disableOnInteraction: false }}
+        loop={true}
+      >
         <SwiperSlide>
-          <div className="page-intro__slide" style={{ backgroundImage: "url('/images/slide-1.jpg')" }}>
+          <div
+            className="page-intro__slide"
+            style={{ backgroundImage: "url('/images/slide-1.jpg')" }}
+          >
             <div className="container">
               <div className="page-intro__slide__content">
                 <h2>JEWELRY</h2>
-                <h3>Handcrafting the world’s best diamonds starts with knowing where they come from. We proudly trace 100% of our rough diamonds to known mines and sources.</h3>
+                <h3>
+                  Handcrafting the world’s best diamonds starts with knowing
+                  where they come from. We proudly trace 100% of our rough
+                  diamonds to known mines and sources.
+                </h3>
                 {/* <a href="#" className="btn-shop"><i className="icon-right"></i>서울지점 바로가기</a> */}
               </div>
             </div>
@@ -21,11 +38,18 @@ const PageIntro = () => {
         </SwiperSlide>
 
         <SwiperSlide>
-          <div className="page-intro__slide" style={{ backgroundImage: "url('/images/slide-2.jpg')" }}>
+          <div
+            className="page-intro__slide"
+            style={{ backgroundImage: "url('/images/slide-2.jpg')" }}
+          >
             <div className="container">
               <div className="page-intro__slide__content">
                 <h2>JEWELRY</h2>
-                <h3>Handcrafting the world’s best diamonds starts with knowing where they come from. We proudly trace 100% of our rough diamonds to known mines and sources.</h3>
+                <h3>
+                  Handcrafting the world’s best diamonds starts with knowing
+                  where they come from. We proudly trace 100% of our rough
+                  diamonds to known mines and sources.
+                </h3>
                 {/* <a href="#" className="btn-shop"><i className="icon-right"></i>서울지점 바로가기</a> */}
               </div>
             </div>
@@ -33,20 +57,26 @@ const PageIntro = () => {
         </SwiperSlide>
 
         <SwiperSlide>
-          <div className="page-intro__slide" style={{ backgroundImage: "url('/images/slide-3.jpg')" }}>
+          <div
+            className="page-intro__slide"
+            style={{ backgroundImage: "url('/images/slide-3.jpg')" }}
+          >
             <div className="container">
               <div className="page-intro__slide__content">
                 <h2>JEWELRY</h2>
-                <h3>Handcrafting the world’s best diamonds starts with knowing where they come from. We proudly trace 100% of our rough diamonds to known mines and sources.</h3>
+                <h3>
+                  Handcrafting the world’s best diamonds starts with knowing
+                  where they come from. We proudly trace 100% of our rough
+                  diamonds to known mines and sources.
+                </h3>
                 {/* <a href="#" className="btn-shop"><i className="icon-right"></i>서울지점 바로가기</a> */}
               </div>
             </div>
           </div>
         </SwiperSlide>
       </Swiper>
-
     </section>
-  )
+  );
 };
 
-export default PageIntro
+export default PageIntro;
